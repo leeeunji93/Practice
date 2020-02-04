5bd39d5c06e3ead2ec612adb12f522083cf6f4dd
leeeunji93 <59554635+leeeunji93@users.noreply.github.com>
2020-02-04 21:32:17 +0900

    2st commit: To-DO-List_ Modify
    To-DO-List 수정 및 추가
    1.코드를 안보고 직접 침
    2.css 코드를 간결하게 구성하는 방법 앎
    font:2em "Intro Inline","Noto Sans KR Medium";
    padding:0px 10px 10px 0px;
    3.변수의 대한 이해를 함 이해 안된 부분은 //로 표시

---
+++ app_2.js
@@ -0,0 +1,61 @@
+loadEvents();
+
+function loadEvents(){
+    document.querySelector('form').addEventListener('submit',submit);
+    document.getElementById('clear').addEventListener('click',clearList)
+    document.querySelector('ul').addEventListener('click',deleteOrTick);
+}
+
+function submit(e){
+    e.preventDefault();
+    let input = document.querySelector('input');
+    if(input.value != '')
+        addTask(input.value);
+    input.value = '';
+}
+
+function addTask(task){
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+    li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;
+    ul.appendChild(li);
+    document.querySelector('.tasksBoard').style.display = 'block';
+}
+
+function clearList(e){
+    let ul=document.querySelector('ul').innerHTML='';
+}
+
+function deleteOrTick(e){
+    if(e.target.className == 'delete')
+        deleteTask(e);
+    else {
+        tickTask(e);
+    }
+}
+//만약에 e(ul태그가 클릭 됐을때)가 타겟팅된 클래스네임이 delete라면 델리트테스크로 가랏
+//아니면 틱테스크로~이벤트 추가할때 ul태그에서 체크할걸 x아니면 체크박스뿐이라는걸 기억하셈
+
+function deleteTask(e){
+    let remove = e.target.parentNode;
+    let parentNode = remove.parentNode;
+    parentNode.removeChild(remove);
+}
+//deleteTask함수는 {
+// e(=deletetask)함수의 타겟팅된 부모는 remove 변수에 들감
+// paranode=remove의 부모노드 들가라}
+
+
+function tickTask(e){
+    const task = e.target.nextSibling;
+    if(e.target.checked){
+        task.style.textDecoration = "line-through";
+        task.style.color = "#a9210b";
+    }else {
+        task.style.textDecoration = "none";
+        task.style.color = "#000000";
+        console.log(e.target);
+    }
+}
+//task변수엔 ticktask의 타겟팅된 옆에 위치한애
+//만약 틱테스크 옆에 타겟팅된 애가 체크 되면~~해라
